f3d1f80dfa1a4b375e6a23ae50d13c775360209f
aelaa <aelaau@gmail.com>
2017-05-15 20:35:08 +0400

    Add player

--- main.lua
+++ main.lua
@@ -1,46 +1,88 @@
-state = {}
+world = {}
+local image
 
+-- third parties
+local anim8 = require('lib/anim8')
+
+-- local
 local menu = require('menu')
 local game = require('game')
 
 function love.load()
-  platform.width = love.graphics.getWidth()
-  platform.height = love.graphics.getHeight()
 
-  platform.x = 0
-  platform.y = platform.height / 2
+  world.current = 'game'
+  world.time_factor = 1
+
+  world['player'] = {}
+
+  player = world.player
 
   player.x = love.graphics.getWidth() / 2
   player.y = love.graphics.getHeight() / 2
-  player.img = love.graphics.newImage('player.png')
+  player.state = 'waiting'
 
-  enemy.x = love.graphics.getWidth() / 2 + 200
-  enemy.y = love.graphics.getHeight() / 2
-  enemy.img = love.graphics.newImage('player.png')
-  state.attack = 0
+  image = love.graphics.newImage('resources/player_waiting.png')
+  local g = anim8.newGrid(32, 32, image:getWidth(), image:getHeight())
+  player.animation = anim8.newAnimation(g('1-7',1), 0.1)
 
   player.speed = 200
   player.gravity = -500
   player.ground = player.y
   player.y_velocity = 0
   player.jump_height = -300
-  player.time_factor = 1
-
-  state.current = 'game'
 end
 
 function love.update(dt)
-  if state.current == 'menu' then
-    menu.update(dt)
-  elseif state.current == 'game' then
-    game.update(dt)
+  -- if state.current == 'menu' then
+  --   menu.update(dt)
+  -- elseif state.current == 'game' then
+  --   game.update(dt)
+  -- end
+  if love.keyboard.isDown('d') then
+    if world.player.state ~= 'walking' then
+      image = love.graphics.newImage('resources/player_walking.png')
+      local g = anim8.newGrid(32, 32, image:getWidth(), image:getHeight())
+      world.player.animation = anim8.newAnimation(g('1-6',1), 0.05)
+      world.player.state = 'walking'
+      world.player.direction = 0
+    end
+
+    player.x = player.x + (player.speed * dt)
   end
+
+  if love.keyboard.isDown('a') then
+    if world.player.state ~= 'walking' then
+      image = love.graphics.newImage('resources/player_walking.png')
+      local g = anim8.newGrid(32, 32, image:getWidth(), image:getHeight())
+      world.player.animation = anim8.newAnimation(g('1-6',1), 0.05):flipH()
+      world.player.state = 'walking'
+      world.player.direction = 1
+    end
+
+    player.x = player.x - (player.speed * dt)
+  end
+
+  if not love.keyboard.isDown('a', 'd') then
+    if world.player.state ~= 'waiting' then
+      image = love.graphics.newImage('resources/player_waiting.png')
+      local g = anim8.newGrid(32, 32, image:getWidth(), image:getHeight())
+      world.player.animation = anim8.newAnimation(g('1-6',1), 0.1)
+      if world.player.direction == 1 then
+        world.player.animation = world.player.animation:flipH()
+      end
+
+      world.player.state = 'waiting'
+    end
+  end
+
+  player.animation:update(dt)
 end
 
 function love.draw()
-  if state.current == 'menu' then
-    menu.draw()
-  elseif state.current == 'game' then
-    game.draw()
-  end
+  player.animation:draw(image, player.x, player.y, 0, 4, 4)
+  -- if state.current == 'menu' then
+  --   menu.draw()
+  -- elseif state.current == 'game' then
+  --   game.draw()
+  -- end
 end
